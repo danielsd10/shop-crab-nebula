170446dc55df4198df7b6c3ac0e1d28d757e1a85
Daniel Salas <danielsd10@gmail.com>
2015-07-01 01:46:13 -0500

    administrador: rutas de categorías

--- routes/admin.js
+++ routes/admin.js
@@ -9,8 +9,30 @@ router.get('/', function(req, res, next) {
 });
 
 /* categorías */
+
+// listar categorías
 router.get('/categories', function(req, res, next) {
 	res.render('admin/categories');
+});
+// formulario de registro
+router.get('/categories/create', function(req, res, next) {
+	res.render('admin/categories-edit');
+});
+// formulario de edición
+router.get('/categories/:id', function(req, res, next) {
+	res.render('admin/categories-edit');
+});
+// guardar nueva categoría
+router.post('/categories', function(req, res, next) {
+	res.redirect('admin/categories');
+});
+// modificar categoría existente
+router.post('/categories/:id', function(req, res, next) {
+	res.redirect('admin/categories');
+});
+// eliminar categoría
+router.delete('/categories/:id', function(req, res, next) {
+	res.status(204);
 });
 
 /* GET producto */
